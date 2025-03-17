fba7e7c659efbe350fe6e0b8e0a2e258bfaab6ab
lillianbaba957 <lillianbaba957@gmail.com>
2025-03-17 22:34:04 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,10 @@
+
+<?php
+
+// This is a PHP script that generates a random number between 1 and 10
+$randomNumber = rand(1, 10);
+
+// Print the result to the screen
+echo "The random number is: $randomNumber";
+
+?>
